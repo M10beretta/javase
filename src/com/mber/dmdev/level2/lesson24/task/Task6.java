56eb2f5f11c4c0f4786d4cbfab2ec787f8e981ec
M10beretta <mberetta.gm@gmail.com>
2021-02-21 00:52:11 +0300

    commit

---
+++ src/com/mber/dmdev/level2/lesson24/task/Task6.java
@@ -0,0 +1,20 @@
+package com.mber.dmdev.level2.lesson24.task;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+/**
+ * 5. Даны два объекта LocalDate из предыдущего задания. Подсчитать количество
+ * секунд между полуночью первой даты и полуночью второй даты.
+ */
+public class Task6 {
+    public static void main(String[] args) {
+        LocalDate now = LocalDate.now();
+        LocalDate prevDate = LocalDate.of(2018, 7, 7);
+        LocalDateTime localDateTimeNow = now.atStartOfDay();
+        LocalDateTime localDateTimePrev = prevDate.atStartOfDay();
+        Duration between = Duration.between(localDateTimePrev, localDateTimeNow);
+        System.out.println(between.getSeconds());
+    }
+}
