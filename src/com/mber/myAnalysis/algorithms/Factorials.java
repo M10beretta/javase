183b5fb25815fb395cc8ab6470aa7e39147bafed
M10beretta <mberetta.gm@gmail.com>
2021-05-26 22:21:26 +0300

    commit

---
+++ src/com/mber/myAnalysis/algorithms/Factorials.java
@@ -0,0 +1,23 @@
+package com.mber.myAnalysis.algorithms;
+
+import java.util.stream.IntStream;
+
+public class Factorials {
+
+    private static int factorialStream(int num) {
+        return IntStream.rangeClosed(1, num).reduce(1, (x, y) -> x * y);
+    }
+
+    private static int factorialLegacy(int num) {
+        int result = 1;
+        for (int i = 2; i <= num; i++) result *= i;
+        return result;
+    }
+
+    private static int factorialRecursive(int num) {
+        if (num <= 2) {
+            return num;
+        }
+        return num * factorialRecursive(num - 1);
+    }
+}
