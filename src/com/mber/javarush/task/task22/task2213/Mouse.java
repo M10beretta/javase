fdbc9d6a9f487d7b06f754f21f7d763c091e535f
M10beretta <mberetta.gm@gmail.com>
2021-07-27 16:09:04 +0300

    commit

---
+++ src/com/mber/javarush/task/task22/task2213/Mouse.java
@@ -0,0 +1,19 @@
+package com.mber.javarush.task.task22.task2213;
+
+public class Mouse {
+    private final int x;
+    private final int y;
+
+    public Mouse(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
---
+++ src/com/mber/javarush/task/task22/task2213/SnakeDirection.java
@@ -0,0 +1,5 @@
+package com.mber.javarush.task.task22.task2213;
+
+enum SnakeDirection {
+    UP, DOWN, LEFT, RIGHT
+}
