70cd01375dfe189ec445fbf9c49be3a910e5eb7d
M10beretta <mberetta.gm@gmail.com>
2021-08-01 12:17:46 +0300

    commit

--- src/com/mber/javarush/task/task36/Task3608/model/FakeModel.java
+++ src/com/mber/javarush/task/task36/Task3608/model/FakeModel.java
@@ -1,7 +1,6 @@
 package com.mber.javarush.task.task36.Task3608.model;
 
 import com.mber.javarush.task.task36.Task3608.bean.User;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,15 +8,15 @@ public class FakeModel implements Model {
     ModelData modelData = new ModelData();
 
     @Override
-    public ModelData getModelData() {
-        return modelData;
-    }
-
-    @Override
     public void loadUsers() {
         List<User> users = new ArrayList<>();
         users.add(new User("Tom", 1L, 1));
         users.add(new User("Mike", 2L, 1));
         modelData.setUsers(users);
+    }
+
+    @Override
+    public ModelData getModelData() {
+        return modelData;
     }
 }
--- src/com/mber/javarush/task/task36/Task3608/view/UsersView.java
+++ src/com/mber/javarush/task/task36/Task3608/view/UsersView.java
@@ -9,8 +9,8 @@ public class UsersView implements View {
     @Override
     public void refresh(ModelData modelData) {
         System.out.println("All users: ");
-        modelData.getUsers().forEach(x -> System.out.println("   " + x));
-        System.out.println("=============================");
+        modelData.getUsers().forEach(x -> System.out.println("\t" + x));
+        System.out.println("==================================================");
     }
 
     @Override
