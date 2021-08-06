de52a1940ba1cac58c53d09ce36d46b27e0f27ca
M10beretta <mberetta.gm@gmail.com>
2021-08-06 18:43:12 +0300

    commit

---
+++ src/com/mber/husivm/multithreading/lesson072_synchronized_collections/Main.java
@@ -0,0 +1,42 @@
+package com.mber.husivm.multithreading.lesson072_synchronized_collections;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        var nameList = new NameList();
+        nameList.add("first");
+
+        class MyThread extends Thread {
+            @Override
+            public void run() {
+                System.out.println(nameList.removeFirst());
+            }
+        }
+
+        MyThread myThread = new MyThread();
+        myThread.setName("one");
+        myThread.start();
+
+        new MyThread().start();
+    }
+
+    static class NameList {
+        List list = Collections.synchronizedList(new ArrayList<>());
+
+        public void add(String name) {
+            list.add(name);
+        }
+
+        public synchronized String removeFirst() {
+            if (list.size() > 0) {
+                if(Thread.currentThread().getName().equals("one")) Thread.yield();
+                return (String) list.remove(0);
+            }
+            return null;
+        }
+    }
+}
