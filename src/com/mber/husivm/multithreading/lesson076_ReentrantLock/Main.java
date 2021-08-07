719dd0965858a3c9d6bafdb4bcb6fc6b24a72fc2
M10beretta <mberetta.gm@gmail.com>
2021-08-07 13:41:54 +0300

    commit

---
+++ src/com/mber/husivm/multithreading/lesson076_ReentrantLock/Main.java
@@ -0,0 +1,72 @@
+package com.mber.husivm.multithreading.lesson076_ReentrantLock;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i < 10; i++) {
+            Resource resource = new Resource();
+            resource.i = 0;
+            resource.j = 0;
+
+            MyThreat myThreat1 = new MyThreat();
+            MyThreat myThreat2 = new MyThreat();
+
+            myThreat1.setResource(resource);
+            myThreat2.setResource(resource);
+
+            myThreat1.start();
+            myThreat2.start();
+
+            myThreat1.join();
+            myThreat2.join();
+
+            System.out.println(resource.i);
+            System.out.println(resource.j);
+        }
+    }
+
+    static class MyThreat extends Thread {
+        private Resource resource;
+
+        public void setResource(Resource resource) {
+            this.resource = resource;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 100000000; i++) {
+                resource.changeI();
+            }
+        }
+    }
+}
+
+
+class Resource {
+    int i;
+    int j;
+
+    Lock lock = new ReentrantLock();
+
+    public void changeI() {
+        lock.lock();
+
+        int i = this.i;
+        i++;
+        this.i = i;
+
+        changeJ();
+
+    }
+
+    public void changeJ() {
+        int j = this.j;
+        j++;
+        this.j = j;
+
+        lock.unlock();
+    }
+
+}
---
+++ src/com/mber/husivm/multithreading/lesson078_Conditions/Main.java
@@ -0,0 +1,50 @@
+package com.mber.husivm.multithreading.lesson078_Conditions;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 20; i++) {
+            new Main.AccountPlus().start();
+            new Main.AccountMinus().start();
+        }
+    }
+
+
+    static Lock lock = new ReentrantLock();
+    static Condition condition = lock.newCondition();
+    static int account = 0;
+
+    static class AccountPlus extends Thread {
+        @Override
+        public void run() {
+            lock.lock();
+            account += 1;
+            System.out.println(account + " after AccountPlus");
+            condition.signal();
+            lock.unlock();
+        }
+    }
+
+    static class AccountMinus extends Thread {
+        @Override
+        public void run() {
+            lock.lock();
+            if (account < 1) {
+                try {
+                    System.out.println(account + " before condition");
+                    condition.await();
+                    System.out.println(account + " after condition");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            account -= 1;
+            System.out.println(account + " after AccountMinus");
+            lock.unlock();
+        }
+    }
+}
