0478d599c5d4cdbbdc727d3d55b2c4642858b268
M10beretta <mberetta.gm@gmail.com>
2021-08-07 17:27:19 +0300

    commit

---
+++ src/com/mber/husivm/multithreading/lesson084_Exchenger/Main.java
@@ -0,0 +1,50 @@
+package com.mber.husivm.multithreading.lesson084_Exchenger;
+
+import java.util.concurrent.Exchanger;
+
+public class Main {
+    public static void main(String[] args) {
+        Exchanger<String> exchanger = new Exchanger<>();
+        new Mike(exchanger);
+        new Form(exchanger);
+    }
+
+    static class Mike extends Thread {
+        Exchanger<String> exchanger;
+
+        public Mike(Exchanger<String> exchanger) {
+            this.exchanger = exchanger;
+            start();
+        }
+
+        @Override
+        public void run() {
+            try {
+                exchanger.exchange("Hi my name is Mike");
+                sleep(3000);
+                exchanger.exchange("I'm 20 years old");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    static class Form extends Thread{
+        Exchanger<String> exchanger;
+
+        public Form(Exchanger<String> exchanger) {
+            this.exchanger = exchanger;
+            start();
+        }
+
+        @Override
+        public void run() {
+            try {
+                System.out.println(exchanger.exchange(null));
+                System.out.println(exchanger.exchange(null));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
